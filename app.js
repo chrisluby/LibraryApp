4ce14ed97e4326eb15b69979294d4cf71789d8c8
Christopher Luby <chrismluby@gmail.com>
2019-08-01 04:20:28 -0400

    button functionality

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 "use strict";
-let myLibrary = [];
+let libr = new Array;
+libr += new Book('Harry Potter', 132, "J.K Bowling Ball", false);
 let addButton = document.querySelector('.button button')
 let form = document.querySelector('.form-style-8')
 let closeBtn = document.querySelector('.close')
+let userFormInput = document.querySelector("form");
 
 
 addButton.addEventListener('click', function(){
-  if (form.style.display === "none") {
-    form.style.display = "block";
-  } else {form.style.display = "none";}
+  if (form.style.display = "none") {form.style.display = "block"};
+  addButton.style.display = "none";
+    
 })
 
 closeBtn.addEventListener('click', function(){
   form.style.display = "none"
+  addButton.style.display = "block";
 })
+
+// form.addEventListener('submit', function(){
+  
+  
+//   e.preventDefault
+//   const name = document.forms['form'].value;
+//   console.log(name)
+// })
+
+function get(){
+  var name = document.getElementById("name").value;
+  var author = document.getElementById("author").value;
+  var pages = document.getElementById("pages").value;
+  var read = document.getElementById("check-one").checked;
+  document.querySelector("form").reset();
+
+  myLibrary += new Book(name, author, pages, read);
+  
+  console.log(name, author, pages, read);
+
+}
+
 
 
 function Book(title, pages, author, read) {
@@ -31,17 +56,21 @@ function Book(title, pages, author, read) {
 };
 
     //* temp book
-const book1 = new Book('Harry Potter', 132, "J.K Bowling Ball", false);
 
-function addBookToLibrary() {
-  // do stuff here
-}
+
+
+
+ 
 
 //sets up page HTML
 function render() {
 
 }
 
+
+libr.forEach(function(element) {
+  console.log(element);
+});
 
 
 
