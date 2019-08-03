95c13b71daf5abe9c3857038032138bcc6556cd8
Christopher Luby <chrismluby@gmail.com>
2019-08-02 20:17:39 -0400

    render function added

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 "use strict";
-let libr = new Array;
-libr += new Book('Harry Potter', 132, "J.K Bowling Ball", false);
+const myLibrary = [];
+let index = 0;
 let addButton = document.querySelector('.button button')
 let form = document.querySelector('.form-style-8')
 let closeBtn = document.querySelector('.close')
 let userFormInput = document.querySelector("form");
+let ul = document.querySelector("ul");
+
+
+//* temp books
+myLibrary.push(new Book('Harry Potter', 132, "J.K Bowling Ball", false));
+myLibrary.push(new Book('Circe', 387, "Adelaine Miller", true));
+myLibrary.push(new Book('Spooky Book ', 11, "Scary Guy", false));
+//* ---------------------------------------------------------------------
 
 
 addButton.addEventListener('click', function(){
@@ -18,13 +26,19 @@ closeBtn.addEventListener('click', function(){
   addButton.style.display = "block";
 })
 
-// form.addEventListener('submit', function(){
-  
-  
-//   e.preventDefault
-//   const name = document.forms['form'].value;
-//   console.log(name)
-// })
+function Book(title, pages, author, read) {
+  this.title = title;
+  this.pages =  pages;
+  this.author = author;
+  if (this.read = true) {
+    this.read = "Yes"
+  } else {this.read = "No"}
+  this.index = index;
+  index++;
+  this.bangRead = function() {
+    this.read = !this.read
+  }
+};
 
 function get(){
   var name = document.getElementById("name").value;
@@ -33,45 +47,47 @@ function get(){
   var read = document.getElementById("check-one").checked;
   document.querySelector("form").reset();
 
-  myLibrary += new Book(name, author, pages, read);
+  var addBook = new Book(name, author, pages, read);
+  myLibrary.push(addBook)
+  
   
   console.log(name, author, pages, read);
-
 }
 
+// console.log(myLibrary)
 
-
-function Book(title, pages, author, read) {
-  this.title = title;
-  this.pages =  pages;
-  this.author = author;
-  this.read = read;
-  this.info = function() {
-    let a = `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'have read!~' : 'not read' }`
-    return a
-  }
-  this.toggleRead = function() {
-    this.read = !this.read //toggles read boolean
-  }
-};
-
-    //* temp book
-
-
-
-
- 
+myLibrary.forEach(function(e, i){
+  console.log(e.title)
+  console.log(i)
+  
+})
 
 //sets up page HTML
 function render() {
+  var li = document.createElement("li");
+  li.classList.add("row");
+  ul.appendChild(li);
+  var div = document.createElement("div");
+  li.appendChild(div)
+  div.classList.add("innercols")
+
+  var p = document.createElement("p");
+  div.appendChild(p);
+  p.innerText = myLibrary[1].title
+
+  var p = document.createElement("p");
+  div.appendChild(p);
+  p.innerText = myLibrary[1].author
+
+  var p = document.createElement("p");
+  div.appendChild(p);
+  p.innerText = myLibrary[1].pages
+
+  var p = document.createElement("p");
+  div.appendChild(p);
+  p.innerText = myLibrary[1].read
 
 }
-
-
-libr.forEach(function(element) {
-  console.log(element);
-});
-
-
+render()
 
 
