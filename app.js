5653dd07900927a803a0537e67f95e83a654a16b
Christopher Luby <chrismluby@gmail.com>
2019-08-03 02:42:28 -0400

    base functionality complete

--- app.js
+++ app.js
@@ -5,14 +5,8 @@ let addButton = document.querySelector('.button button')
 let form = document.querySelector('.form-style-8')
 let closeBtn = document.querySelector('.close')
 let userFormInput = document.querySelector("form");
-let ul = document.querySelector("ul");
 
-
-//* temp books
-myLibrary.push(new Book('Harry Potter', 132, "J.K Bowling Ball", false));
-myLibrary.push(new Book('Circe', 387, "Adelaine Miller", true));
-myLibrary.push(new Book('Spooky Book ', 11, "Scary Guy", false));
-//* ---------------------------------------------------------------------
+let ul = document.querySelector(".inner");
 
 
 addButton.addEventListener('click', function(){
@@ -30,15 +24,51 @@ function Book(title, pages, author, read) {
   this.title = title;
   this.pages =  pages;
   this.author = author;
-  if (this.read = true) {
-    this.read = "Yes"
-  } else {this.read = "No"}
+  this.read = read ? "Yes" : "No";
   this.index = index;
   index++;
-  this.bangRead = function() {
-    this.read = !this.read
+  this.bang = function() {
+    if (this.read === "Yes"){
+      return this.read = "No"
+    } else if (this.read === "No") {
+      return this.read = "Yes"
+    }
   }
 };
+
+//sets up page HTML
+function render() {
+  myLibrary.forEach(function(e, i){
+  var li = document.createElement("li");
+  li.classList.add("row");
+  
+  ul.appendChild(li);
+
+  var div = document.createElement("div");
+  li.appendChild(div)
+  div.classList.add("innercols")
+   
+  var p = document.createElement("p");
+  div.appendChild(p);
+  p.innerText = e.title
+  
+  var p = document.createElement("p");
+  div.appendChild(p);
+  p.innerText = e.author
+  
+  var p = document.createElement("p");
+  div.appendChild(p);
+  p.innerText = e.pages
+  
+  var p = document.createElement("p");
+  p.classList.add('read');
+  p.classList.add(i); //adds to index of the added book to the innercols div as a class.  beginning at 0.
+  div.appendChild(p);
+  p.innerText = e.read
+
+  readClicks();
+  })
+}
 
 function get(){
   var name = document.getElementById("name").value;
@@ -49,45 +79,29 @@ function get(){
 
   var addBook = new Book(name, author, pages, read);
   myLibrary.push(addBook)
-  
-  
-  console.log(name, author, pages, read);
+  clear();
+  render();
 }
 
-// console.log(myLibrary)
-
-myLibrary.forEach(function(e, i){
-  console.log(e.title)
-  console.log(i)
-  
-})
-
-//sets up page HTML
-function render() {
-  var li = document.createElement("li");
-  li.classList.add("row");
-  ul.appendChild(li);
-  var div = document.createElement("div");
-  li.appendChild(div)
-  div.classList.add("innercols")
-
-  var p = document.createElement("p");
-  div.appendChild(p);
-  p.innerText = myLibrary[1].title
-
-  var p = document.createElement("p");
-  div.appendChild(p);
-  p.innerText = myLibrary[1].author
-
-  var p = document.createElement("p");
-  div.appendChild(p);
-  p.innerText = myLibrary[1].pages
-
-  var p = document.createElement("p");
-  div.appendChild(p);
-  p.innerText = myLibrary[1].read
-
+let clear = function() {
+  let rowNodes = document.querySelectorAll('.row');
+  rowNodes.forEach(function(e){
+    e.remove();
+  })
 }
-render()
 
+function readClicks(){
+  let z = document.querySelectorAll('.read')
+  
+  z.forEach(function(ele, i){
+    ele.addEventListener("click", script)
+  })
+}
+
+function script(){
+  myLibrary[this.classList[1]].bang();
+  console.log(this.classList[1])
+  clear();
+  render();
+}
 
